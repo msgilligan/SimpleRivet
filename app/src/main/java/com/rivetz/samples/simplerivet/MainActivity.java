d46557f977c063262ef509b524334bd6c9b38863
hobnobber <markdavid@hoblit.net>
2015-03-06 12:57:56 -0500

    GetKey

--- app/src/main/java/com/rivetz/samples/simplerivet/MainActivity.java
+++ app/src/main/java/com/rivetz/samples/simplerivet/MainActivity.java
@@ -48,13 +48,18 @@ public class MainActivity extends ActionBarActivity {
                         .putExtra(Rivet.EXTRA_MESSAGE,"This is a test");
 
                 startActivityForResult(intent, Rivet.INSTRUCT_SIGN);
+                intent = new Intent(Rivet.RIVET_INTENT)
+                        .putExtra(Rivet.EXTRA_INSTRUCT, Rivet.INSTRUCT_GETKEY)
+                        .putExtra(Rivet.EXTRA_SPID, MYSPID)
+                        .putExtra(Rivet.EXTRA_KEYNAME, keyName);
+                startActivityForResult(intent, Rivet.INSTRUCT_GETKEY);
             } else if (requestCode == Rivet.INSTRUCT_GETKEY) {
                 // change the intent fired to GETKEY to display the public key
                 String pubKey = data.getStringExtra(Rivet.EXTRA_PUBLICDATA);
-                output.append("\npubkey="+pubKey);
+                output.append("\npubkey="+pubKey+"\n");
             } else if (requestCode == Rivet.INSTRUCT_SIGN) {
                 String signature = data.getStringExtra(Rivet.EXTRA_SIGNATURE);
-                output.append("signature="+signature);
+                output.append("\nsignature="+signature+"\n");
             }
         } else {
             output.setText("not done");
